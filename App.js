35a3a053e4b4c50d6b2df451f0c5f795e69d23b3
webdevFaizan <webdev.faizan@gmail.com>
2023-01-16 04:30:21 +0530

    Basic components are added and understood the difference between Native and react components.

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* The style object is created as a form of JS object which will be used to pass to the View component to give it style. */}
+      {/* If we add the string "Hello World" without the Text component then this will throw an error */}
+      <View>
+        <Text>Hello World</Text>
+      </View>
+      <View>
+        <Button
+        onPress={null}
+        title="Learn More"
+        color="#743234"
+        accessibilityLabel="Learn more about this purple button"
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
+// These stylesheet objects are used in the case of React Native applications since there are no option for CSS, since the DOM is not present int the case of Native Applications.
 const styles = StyleSheet.create({
   container: {
     flex: 1,

1063aa43452f1698c0ce76a14d08db6d6d338e7c
webdevFaizan <webdev.faizan@gmail.com>
2023-01-17 11:05:21 +0530

    FlatList is much more optimised than ScrollView, implemented the flatlist completely.

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
+import { 
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+  FlatList    //This flatList is of high importance, since internally this will make the list of large items very much optimised. And the loading will be done only when we scroll near to them, and this loading will be done lazily. Not rendering the whole list during the first render, this will make the app much more optimised. This is how the infinite scroll of facebook and instagram work.
+ } from 'react-native';
 
 //Module 1 -
 // export default function App() {
@@ -41,7 +49,7 @@ export default function App() {
     //IMPORTANT :  The ABOVE ONE is an inefficient code, as we are taking the old list, spreading it then adding a new list and then changing the state. This seems very inefficient because of adding in O(1) by just pushing, we are actually adding in O(N)
     
     // DOUBT AND RESEARCH : How is the below code better and more efficient than the above code?
-    setCourseGoals(currentCourseGoals => [...currentCourseGoals, enteredGoalText]); //This is recommended by the react docs, directly changing the value of the State is not recommended, this should be done using function call.
+    setCourseGoals(currentCourseGoals => [...currentCourseGoals, {data : enteredGoalText, id : Math.random().toString()}]); //This is recommended by the react docs, directly changing the value of the State is not recommended, this should be done using function call.
 
 
   };
@@ -54,14 +62,21 @@ export default function App() {
         <Button onPress={addGoalHandler} title='Add Goal'/>
       </View>
       <View style={{flex : 5}}>
-        <ScrollView>
-        {courseGoals.map((goal)=>
-        <View style={styles.goalItem} key={goal}>
-          {/* IMPORTANT : Since the style element is applied to the View Component, there is an important difference between the actual Cascading Style Sheets and this CSS like JS codes. In here, the style of View is not going to be applied to the children component, and the child component is Text. This is why in order to have specific color of the Texts we need to give it separate color. */}
-          <Text style={styles.goalText}>{goal}</Text>
-        </View>
-       )}
-       </ScrollView>
+        <FlatList
+        alwaysBounceVertical= {true}    //DOUBT : This does not seem to work
+        data={courseGoals}  //IMPORTANT : This shows which data should be rendered, but the renderItem prop tells FlatList, how the data should be rendered. And keep this in mind, this behaviour of renderItem is what is making only the visible partial list rendered, but at the same time, the next items from the long list will only be rendered when we scroll furthur.
+        renderItem={(itemObject)=>{ //One thing to note here is that this itemObject is actually an Object, not a string element. So to extract the data we need to use itemObject.item.data 
+          return (
+            // IMPORTANT : Notice the custom mapping method is now removed which was being used in the case of ScrollView, but instead a separate function is itself created such that the renderItem is now taking an object from the list of arrays and then extracting the data from it and then displaying.
+          <View style={styles.goalItem}>
+            {/* IMPORTANT : Since the style element is applied to the View Component, there is an important difference between the actual Cascading Style Sheets and this CSS like JS codes. In here, the style of View is not going to be applied to the children component, and the child component is Text. This is why in order to have specific color of the Texts we need to give it separate color. */}
+            <Text style={styles.goalText}>{itemObject.item.data}</Text>
+          </View>
+          )
+        }}
+        keyExtractor = {(item,index)=>{return item.id}} //VERY IMPORTANT, PRIORITY : This keyExtractor property is kept separate in the case of FlatList because the key that is coming from the stream of data could be from APIs, and APIS has their own set of rules about what should be the key, so we are extracting it separately.
+        // The reason why this keyExtractor is kept separate, is that each View item from the list will require the unique list item.
+        />
       </View>
     </View>
   );
